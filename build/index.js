cc159e6c84a27e5078164eb4f1b38c3b679ab8c7
RoseMagura <magurarm@guilford.edu>
2021-01-04 21:15:14 -0500

    Feat: Set up basic express server

--- build/index.js
+++ build/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('Hello world!');
+var express = require('express');
+var app = express();
+var port = 8080;
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+app.listen(port, function () {
+    console.log("Server started at http:localhost:" + port);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
+    res.send('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http:localhost:${ port }`);
+});
