62601076646abe8a89e02d3eb9daa62ef097a4a4
RoseMagura <magurarm@guilford.edu>
2021-01-07 14:37:10 -0500

    Edit: Simple version returns processed images

--- src/simple_index.js
+++ src/simple_index.js
@@ -2,6 +2,8 @@ const express = require( "express" );
 const sharp = require("sharp");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const { fileURLToPath } = require("url");
 
 const app = express();
 app.use(cors());
@@ -26,15 +28,33 @@ app.get('/', (req, res) => {
 
 // Handle posting 
 app.post('/', (req, res) => {
-    res.send('OK');
-    // TODO: Check if image already processed
+    const path = `./views/images/${req.body.name}.jpg`;
+    const processed = `./views/processed_images/${req.body.name}${req.body.width}x${req.body.height}.jpg`;
+    // Check if image already processed
+    try {
+        if(fs.existsSync(processed)) {
+            res.send('File already processed. Check output folder.')
+        } else {
+            // If hasn't been processed yet, do now
+            try{ 
+                if(fs.existsSync(path)) {
+                    res.send('Processing image...');
 
-    //TODO: If no image matches, return error message
-
-    // TODO: Make sharp a separate module
-    sharp(`./views/images/${req.body.name}.jpg`)
-        .resize(req.body.width, req.body.height)
-        .toFile(`./views/processed_images/${req.body.name}${req.body.width}x${req.body.height}.jpg`);
+                    // TODO: Make sharp a separate module
+                    sharp(path)
+                        .resize(req.body.width, req.body.height)
+                        .toFile(processed);
+                } else{
+                    // If no image matches, return error message
+                    res.send('File not found. Please double-check spelling.');
+                }
+            } catch (error) {
+                console.log('error', error);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 //TODO: Handle errors
