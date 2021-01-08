2cd1da3f92626926b086889c6c3204a090b2d695
RoseMagura <magurarm@guilford.edu>
2021-01-08 14:53:17 -0500

    Edit: Jest configuration working?

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
-    res.send('Hello world!');
+app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
+  res.send("Hello world!");
 });
 
-
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${ port }`);
+  console.log(`Server started at http://localhost:${port}`);
 });
--- src/js/handle_sharp.js
+++ src/js/handle_sharp.js
@@ -1,16 +1,14 @@
-import fs from 'fs';
-import sharp from 'sharp';
+import fs from "fs";
+import sharp from "sharp";
 
 export const process_image = (name, width, height) => {
-    const path = `./views/images/${name}.jpg`;
-    const processed = `./views/processed_images/${name}${width}x${height}.jpg`;
-    try {
-        sharp(path)
-            .resize(parseInt(width), parseInt(height))
-            .toFile(processed);
-    } catch (error) {
-        console.log('Issue with processing image', error);
-        return 'failure';
-    }
-    return 'Successfully processed image. Check views/processed_images';
-}
+  const path = `./views/images/${name}.jpg`;
+  const processed = `./views/processed_images/${name}${width}x${height}.jpg`;
+  try {
+    sharp(path).resize(parseInt(width), parseInt(height)).toFile(processed);
+  } catch (error) {
+    console.log("Issue with processing image", error);
+    return "failure";
+  }
+  return "Successfully processed image. Check views/processed_images";
+};
